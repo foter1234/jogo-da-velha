29d5ed45ae25fb889356bacf59c374018d206f28
Lucas Pereira Esteves <lucaspesteves2017@gmail.com>
2023-09-03 18:47:03 -0400

    atualizando o jogo da velha

--- js/index.js
+++ js/index.js
@@ -5,15 +5,86 @@ window.onload=()=>{
     }
 }
 
-var jogador= 1;
-var jogada =false
-var casas = new array[0,0,0,0,0,0,0,0,0]
-var marcar = new array()
-var posicao = 0 
+var jogador = 1;
+var casas = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0);
+var marcar = new Array()
+var posicao = 0;
 
-function changeImage() {
-    elem = document.querySelector('.bg-img');
 
-    // Setting the backgroundImage to an url
-    elem.style.backgroundImage = "url('https://cdn.awsli.com.br/600x450/2437/2437567/produto/215734022/5822484585_1-p8irc8tqsy.jpg')";
+function fazerJogada(ncasas) {
+    var casa = document.getElementById(`casa${ncasas}`);
+
+    if (casas[ncasas] === 0) {
+        casas[ncasas] = jogador;
+        marcar.push(ncasas);//o push() adiciona um valor em um array
+
+        if (jogador === 1) {
+            casa.textContent = 'X';
+            jogador = 2;
+        } else {
+            casa.textContent = 'O';
+            jogador = 1;
+        }
+
+        posicao++;
+
+        if (verificar()) {
+            document.getElementById('vencedor').textContent = 'Jogador ' + (jogador === 1 ? 'O' : 'X') + ' venceu!';
+            desabilitar()
+        } else if (posicao === 9) {
+            document.getElementById('vencedor').textContent = 'Empate!';
+            desabilitar()
+        }
+    }
 }
+
+
+function verificar() {
+    var vitoria = false;
+  
+
+    if (casas[0] !== 0) {
+        if (casas[0] === casas[1] && casas[1] === casas[2]) {
+            vitoria = true;
+        }
+        if (casas[0] === casas[3] && casas[3] === casas[6]) {
+            vitoria = true;
+        }
+        if (casas[0] === casas[4] && casas[4] === casas[8]) {
+            vitoria = true;
+        }
+    }
+
+    if (casas[1] !== 0 && casas[1] === casas[4] && casas[4] === casas[7]) {
+        vitoria = true;
+    }
+
+    if (casas[2] !== 0) {
+        if (casas[2] === casas[5] && casas[5] === casas[8]) {
+            vitoria = true;
+        }
+        if (casas[2] === casas[4] && casas[4] === casas[6]) {
+            vitoria = true;
+        }
+    }
+
+    if (casas[3] !== 0 && casas[3] === casas[4] && casas[4] === casas[5]) {
+        vitoria = true;
+    }
+
+    if (casas[6] !== 0 && casas[6] === casas[7] && casas[7] === casas[8]) {
+        vitoria = true;
+    }
+
+    return vitoria;
+}
+
+
+
+function desabilitar() {
+    for (var i = 0; i < 9; i++) {
+        var casa = document.getElementById(`casa${i}`);
+        casa.onclick = null;
+    }
+}
+
